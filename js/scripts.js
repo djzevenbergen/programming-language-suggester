468db3672dd8b0f14c8836729f00ba57b1bf1e9f
djzevenbergen <djzevenbergen@gmail.com>
2020-03-27 11:27:52 -0700

    use array to keep track of answer counts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+var countAnswers = function (answers) {
+  for (i = 0; i < answers.length; i++) {
+    var a = 0;
+    var b = 0;
+    var c = 0;
+    var d = 0;
+    var e = 0;
+    if (answers[i] === 1) {
+      a += 1;
+    } else if (answers[i] === 2) {
+      b += 1;
+    } else if (answers[i] === 3) {
+      c += 1;
+    } else if (answers[i] === 4) {
+      d += 1;
+    } else {
+      e += 1;
+    }
+    var count = [a, b, c, d, e];
+    return count;
+  }
+}
+
+
+
 var computeLanguage = function (aCount, bCount, cCount, dCount) {
   //if more a's than anything, they like python
 
@@ -16,9 +41,6 @@ $(document).ready(function () {
   $("#startButton").click(function () {
     $("#startButton").hide();
     $("#questionOne").slideDown();
-
-
-
 
   });
 
@@ -70,12 +92,23 @@ $(document).ready(function () {
     var a = 0;
     var b = 0;
     var c = 0;
-    var b = 0;
-    //if ($"input:radio[name=")
+    var d = 0;
+    var e = 0;
+    var one = parseInt($('input:radio[name="yeet"]:checked').val());
+    var two = parseInt($('input:radio[name="greet"]:checked').val());
+    var three = parseInt($('input:radio[name="zombie"]:checked').val());
+    var four = parseInt($('input:radio[name="day"]:checked').val());
+    var five = parseInt($('input:radio[name="scream"]:checked').val());
+    var six = parseInt($('input:radio[name="president"]:checked').val());
+    var seven = parseInt($('input:radio[name="toDo"]:checked').val());
+    var name = $("input#uName").val();
+
+    var answers = [one, two, three, four, five, six, seven];
 
 
 
-    //yeet, greet, zombie
+    alert(counts);
+
 
   });
 
