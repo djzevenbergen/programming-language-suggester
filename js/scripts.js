b12ec3c74787191720b7a6b72e0315749542ce91
djzevenbergen <djzevenbergen@gmail.com>
2020-03-27 09:50:14 -0700

    add forms and work on adding questions to html

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var computeLanguage = function (aCount, bCount, cCount, dCount) {
+  //if more a's than anything, they like python
+
+  //else if more b's than anything, they like c#
+
+  // else if more c's than anything, they like ruby
+
+  // else if more d's, they are inhuman and must learn machine code
+
+
+}
+
+
+
+$(document).ready(function () {
+
+})
